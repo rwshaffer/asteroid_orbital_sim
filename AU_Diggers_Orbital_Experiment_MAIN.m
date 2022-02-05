bed8c3138472945ae1af866955c71de5a4919b2a
Alexander Petsopoulos <palex22@vt.edu>
2022-02-05 11:19:18 -0500

    Code is broken asf, I'm trying to insert a planet and it's NOT working

--- AU_Diggers_Orbital_Experiment_MAIN.m
+++ AU_Diggers_Orbital_Experiment_MAIN.m
@@ -1,13 +1,18 @@
+%% Documentation and clear/clc
 %AU Digger Orbital Simulation Experiment MAIN
 %02/05/2022 Alexander Petsopoulos
 %Much of this script is adapted from AGI's
 %'STK_Matlab_Object_Test.m'
+clear, clc
 
+%% 3. Initialize STK instance/scenario
 % Launch a new instance of STK
 uiapp = actxserver('STK12.Application');
 
 % or to grab an already open instance of STK
 %uiapp = actxGetRunningServer('STK11.Application');
+
+%turn off graphics
 
 % Attach to the STK Object Model
 root = uiapp.Personality2;
@@ -15,6 +20,17 @@ root = uiapp.Personality2;
 % Create a new scenario
 root.NewScenario('OrbitalSim')
 % if one already exists, you can access it as in the examples below
+
+%%  4. Populate simulation objects: Earth, asteroid, electric propulsion engine
+
+%Initializing Jupiter as a test for how to create a planet
+% IAgScenario scenario: Scenario object
+planet = root.Children.New('ePlanet', 'Jupiter');
+planet.CommonTasks.SetPositionSourceCentralBody('Jupiter', 'eEphemJPLDE');
+
+%% 5. Insert satellite and configure some settings common to all simulations
+
+%- Turn off any unnecessary graphics to optimize sim. speed
 
 % Create our satellite.
 sat = root.CurrentScenario.Children.New('eSatellite', 'DiggerSat');
