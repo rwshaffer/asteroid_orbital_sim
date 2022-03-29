d77dadb260d853c905752b6bb4bc58e174b4a6ed
Alexander Petsopoulos <palex22@vt.edu>
2022-03-29 17:09:37 -0400

    makin progress

--- save_data.m
+++ save_data.m
@@ -4,18 +4,17 @@ ASTG = sat.Propagator;
 ts_ind = []; % List of indices of target sequences in the MCS
 MCS_items = ASTG.MainSequence.Count; % Number of main segments in the MCS
 for i = 0:MCS_items-1
+    item = ASTG.MainSequence.Item(i);
     % Check if current segment is a target sequence
-    if ASTG.MainSequence.Item(i).Type == "eVASegmentTypeTargetSequence"
-        ts = [ts_ind i]; % Store index of target sequence
-        
+    if item.Type == "eVASegmentTypeTargetSequence"
+        countTS = item.Segments.Count;
+        %% Check if the segments are maneuvers
+        for jj = 0:length(countTS)
+            segment = item.Segments.Item(jj); % Gives you the first item within the target sequence
+            % code to check whether "segment" is a maneuver here
+            isManeuver(i,jj+1) = (segment.Type == "eVASegmentTypeManeuver");
+        end
     end
-    
-end
-for jj = 1:i
-    ts = ASTG.MainSequence.Item(0); % Gives you the target sequence object
-    segment = ts.Segments.Item(0); % Gives you the first item within the target sequence
-    % code to check whether "segment" is a maneuver here
-    
 end
 dataTable = 1;
 end
