a873fc1d9f154576a195d1ffa04fd3fe19752786
Gen Gemond <ggemond1@vt.edu>
2022-03-31 15:00:47 -0400

    deltaV extraction code

--- save_data.m
+++ save_data.m
@@ -3,6 +3,7 @@ ASTG = sat.Propagator;
 %% Identify all target sequences in the MCS
 ts_ind = []; % List of indices of target sequences in the MCS
 MCS_items = ASTG.MainSequence.Count; % Number of main segments in the MCS
+deltaV = 0; 
 for i = 0:MCS_items-1
     item = ASTG.MainSequence.Item(i);
     % Check if current segment is a target sequence
@@ -12,7 +13,11 @@ for i = 0:MCS_items-1
         for jj = 0:length(countTS)
             segment = item.Segments.Item(jj); % Gives you the first item within the target sequence
             % code to check whether "segment" is a maneuver here
-            isManeuver(i,jj+1) = (segment.Type == "eVASegmentTypeManeuver");
+            isManeuver(i+1,jj+1) = (segment.Type == "eVASegmentTypeManeuver");
+     
+            if isManeuver(i+1, jj+1)
+               deltaV = deltaV + segment.GetResultValue('DeltaV');
+            end
         end
     end
 end
